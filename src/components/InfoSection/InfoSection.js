48d78213fb5aa47eadf4206ee97f073ce0b77912
Ahmet Berdiyev <berdiyew707@gmail.com>
2021-06-23 17:15:21 +0300

    complement Info section

--- src/components/InfoSection/InfoSection.js
+++ src/components/InfoSection/InfoSection.js
@@ -1,15 +1,44 @@
 import React from 'react'
-import { Container } from '../../globalStyles';
-import { InfoColumn, InfoRow, InfoSec, TextWrapper } from './InfoSection.elements';
+import { Link } from 'react-router-dom';
+import { Container, Button } from '../../globalStyles';
+import {
+   InfoColumn,
+   InfoRow,
+   InfoSec,
+   TextWrapper,
+   TopLine,
+   Heading,
+   Subtitle
+} from './InfoSection.elements';
 
-const InfoSection = ({ lightBg, imgStart }) => {
+const InfoSection = ({
+   lightBg,
+   imgStart,
+   lightTopLine,
+   lightText,
+   lightTextDesc,
+   buttonLabel,
+   description,
+   headLine,
+   primary,
+   topLine
+}) => {
    return (
       <>
          <InfoSec lightBg={lightBg} >
             <Container>
                <InfoRow imgStart={imgStart}>
                   <InfoColumn>
-                     <TextWrapper>h</TextWrapper>
+                     <TextWrapper>
+                        <TopLine lightTopLine={lightTopLine}>{topLine}</TopLine>
+                        <Heading lightText={lightText}>{headLine}</Heading>
+                        <Subtitle lightTextDesc={lightTextDesc}>{description}</Subtitle>
+                        <Link>
+                           <Button big fontBig primary={primary}>
+                              {buttonLabel}
+                           </Button>
+                        </Link>
+                     </TextWrapper>
                   </InfoColumn>
                </InfoRow>
             </Container>
