45b1e8631144da5a715f577cfdbb52e0b5c92c95
Ahmet Berdiyev <berdiyew707@gmail.com>
2021-06-23 17:13:11 +0300

    data complement

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,3 +1,14 @@
 export const homeObjOne = {
-   lightBg: false
+   lightBg: false,
+   imgStart: '',
+   lightTopLine: true,
+   lightText: true,
+   lightTextDesc: true,
+   buttonLabel: 'Get Started',
+   description:
+      'We help business owners increase their revenue. Our team of unique specialist can hel you acheive your business goals.',
+   headLine:
+      'Lead Generation Specialist for Online Business',
+   primary: true,
+   topLine: 'Margeting Agency'
 }
